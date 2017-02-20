5f776a82d85a23990309f481107a660b20a70f51
root <root@localhost.localdomain>
2017-02-19 22:18:23 -0500

    Added menu

--- autobeef.sh
+++ autobeef.sh
@@ -4,12 +4,42 @@ get_token="$(curl -H "Content-Type: application/json" -X POST -d '{"username":"b
 
 token="$(echo "$get_token" | awk -F '["]' '{print $6}')"
 
-get_sessions="$(curl http://127.0.0.1:3000/api/hooks?token=$token | json_pp)"
+function select_target {
+	get_sessions="$(curl http://127.0.0.1:3000/api/hooks?token=$token | json_pp)"
+	echo "$get_sessions"
+	echo -n "Enter a Session: "
+	read session 
+}
 
-echo "$get_sessions"
+function about_browser {
+	curl http://127.0.0.1:3000/api/hooks/${session}?token=${token} | json_pp
+	read -n1 -r -p "Press space to continue..." key	
+}
 
-echo -n "Enter a Session: "
+while :
+do
+    clear
+    cat<<EOF
+    ==============================
+    autoBeEF v1.0
+    ------------------------------
+    Please enter your choice:
 
-read session 
-
-curl http://127.0.0.1:3000/api/hooks/${session}?token=${token} | json_pp
+    Select Target     (1)
+    About Target      (2)
+    Persistance       (3)
+    Attack            (4)
+                      (Q)uit
+    ------------------------------
+EOF
+    read -n1 -s
+    case "$REPLY" in
+    "1")  select_target             ;;
+    "2")  about_browser             ;;
+    "3")  echo "you chose choice 3" ;;
+    "Q")  exit                      ;;
+    "q")  exit                      ;; 
+     * )  echo "invalid option"     ;;
+    esac
+    sleep 1
+done
